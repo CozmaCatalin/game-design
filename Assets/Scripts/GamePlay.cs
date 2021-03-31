b61b82d1be2de1c3f194aef9862fdc9a4c306571
Cozma Catalint <cozmacatalin33@gmail.com>
2021-03-31 22:09:42 +0300

    d

--- Assets/Scripts/GamePlay.cs
+++ Assets/Scripts/GamePlay.cs
@@ -6,6 +6,7 @@ public class GamePlay : MonoBehaviour
 {
     // Start is called before the first frame update
     public Transform[] spawnPositions;
+    public GameObject[] enemyPrefabs;
     public int positionsNumber = 4;
     public int wave = 1;
 
@@ -17,6 +18,13 @@ public class GamePlay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.L))
+        {
+            Debug.Log("Enemy spawned!");
+            int randEnemy = Random.Range(0, enemyPrefabs.Length);
+            int randSpawnPoint = Random.Range(0, spawnPositions.Length);
+
+            Instantiate(enemyPrefabs[0], spawnPositions[randSpawnPoint].position, transform.rotation);
+        }
     }
 }
